58fc61672bc96d2059d03f5c46a3051d3757c060
Will Breiler <will.breiler@gmail.com>
2022-06-13 23:14:55 -0500

    Boobies

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Will Breiler on 6/13/22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+		VStack {
+			ProgressView(value: 5, total: 15)
+			HStack {
+				VStack(alignment: .leading) {
+					Text("Seconds elapsed")
+						.font(.caption)
+					Label("300", systemImage: "hourglass.bottomhalf.fill")
+				}
+				Spacer()
+				VStack(alignment: .trailing) {
+					Text("Seconds remaining")
+						.font(.caption)
+					Label("600", systemImage: "hourglass.tophalf.fill")
+				}
+			}
+			Circle()
+				.strokeBorder(lineWidth: 24)
+			HStack {
+				Text("Speaker 1 of 3")
+				Button(action: {}) {
+					Image(systemName: "forward.fill")
+				}
+			}
+		}
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
