6656daa6fdccd9f02cad219c4cbad806237e1f49
Will Breiler <will.breiler@gmail.com>
2022-06-17 15:24:24 -0500

    Suck me

---
+++ Scrumdinger/MeetingHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Will Breiler on 6/17/22.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView()
+    }
+}
