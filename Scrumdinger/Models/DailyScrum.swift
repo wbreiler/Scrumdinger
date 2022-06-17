887bc9a32a343a80467f92da321bdf5ecc683a3a
Will Breiler <will.breiler@gmail.com>
2022-06-16 22:09:10 -0500

    Fixed bugs

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -35,10 +35,14 @@ extension DailyScrum {
     }
     
     struct Data {
-        var title: String
-        var attendees: [Attendee]
-        var lengthInMinutes: Double
-        var theme: Theme
+        var title: String = ""
+        var attendees: [Attendee] = []
+        var lengthInMinutes: Double = 5
+        var theme: Theme = .seafoam
+    }
+    
+    var data: Data {
+        Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), theme: theme)
     }
 }
 
